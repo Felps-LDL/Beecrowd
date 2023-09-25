4b668484d3b260f4dc0b0a120a3cdc5ece16bc1f
Felipe Lages de Lima <106702104+Felps-LDL@users.noreply.github.com>
2023-09-25 13:13:01 -0300

    Create 3344.cpp
    Beecrowd 3344 - Brute

---
+++ 3344.cpp
@@ -0,0 +1,29 @@
+// beecrowd 3344 - Brute
+// Felipe Lages de Lima
+// Felps-LDL
+
+#include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define MAXN 10100
+#define INTMAX 10000000000
+#define pii pair<int, int> 
+#define delta 0.0000001
+#define ALL(x) x.abegin(), x.end()
+#define ll long long
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    optimize;
+
+    cin >> N;
+
+    // sempre vai dar 4 depois de uma certa quantidade de operações!
+    cout << 4 << endl;
+
+    return 0;
+}
