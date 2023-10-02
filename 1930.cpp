29f007ee0837b1f8cd1f582f96ba7e6c4a5c9c4e
Felipe Lages de Lima <106702104+Felps-LDL@users.noreply.github.com>
2023-10-02 18:51:30 -0300

    Create 1930.cpp
    Beecrowd 1930 - Tomadas

---
+++ 1930.cpp
@@ -0,0 +1,25 @@
+// beecrowd 1930 - Tomadas
+// Felipe Lages de Lima
+// Felps-LDL
+
+#include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define MAXN 10100
+#define INTMAX 10000000000
+#define pii pair<int, int> 
+#define delta 0.0000001
+#define ALL(x) x.begin(), x.end()
+
+using namespace std;
+
+int T1, T2, T3, T4;
+
+int main()
+{
+    optimize;
+
+    cin >> T1 >> T2 >> T3 >> T4;
+    cout << T1 + T2 + T3 + T4 - 3 << endl;
+    return 0;
+}
