e0a8dd5f791ddd69ebd91b2c53e991948d9dc09d
Felipe Lages de Lima <106702104+Felps-LDL@users.noreply.github.com>
2024-03-22 10:49:43 -0300

    Create 1323.cpp

---
+++ 1323.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define f first
+#define s second
+#define dbg(x) cout << "x = " << x << endl;
+#define ll long long
+#define pii pair<int, int> 
+#define piii pair<pair<int,int>, int>
+#define ALL(x) x.begin(), x.end()
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3fll
+
+using namespace std; 
+
+int N;
+
+int main()
+{
+    optimize;
+
+    while(true)
+    {
+        cin >> N;
+        if (N == 0) break;
+        cout << (N * (N + 1) * (N * 2 + 1)) / 6 << endl;
+    }
+
+
+    return 0;
+}
